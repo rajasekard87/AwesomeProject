446486f871c06d199bf37394f5822273888f4a84
rajasekard87 <100590877+rajasekard87@users.noreply.github.com>
2023-04-02 20:04:12 +0530

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -1,39 +1,10 @@
-import React, { useState } from 'react';
-import {
-SafeAreaView,
-ScrollView,
-StatusBar,
-StyleSheet,
-Text,
-useColorScheme,
-View,
-TextInput,
-TouchableOpacity,
-} from 'react-native';
-import {
-Colors,
-DebugInstructions,
-Header,
-LearnMoreLinks,
-ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-import Login from './src/screens/login.screen';
-const App =  () => {
-const onPressLogin = () => {
-// Do something about login operation
-};
-const onPressForgotPassword = () => {
-// Do something about forgot password operation
-};
-const onPressSignUp = () => {
-// Do something about signup operation
-};
-const [state,setState] = useState({
-email: '',
-password: '',
-})
-return (
-  <Login login={undefined}/> 
- );
-}
+import React, { useState } from 'react'; 
+import Login from './src/screens/login.screen'; 
+const App = () => { 
+  
+  return ( 
+    <Login login={undefined}/> 
+  ); 
+} 
 
+export default App;
